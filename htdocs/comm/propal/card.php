a53dcf34954ead68ee327dca2426dc290cb79ba7
yannis <yannishoareau26@gmail.com>
2025-02-03 15:58:01 +0100

    phan fix

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -3366,13 +3366,17 @@ if ($action == 'create') {
 					// Array of the subbuttons
 					$url_button = array(
 						array(
+							'lang' => 'subtotals',
 							'label' => $langs->trans('Title'),
 							'perm' => true,
+							'enabled' => (isModEnabled('order') && $object->status == Propal::STATUS_DRAFT),
 							'urlraw' => $_SERVER["PHP_SELF"].'?id='.$object->id.'&action=add_title_line&token='.newToken()
 						),
 						array(
+							'lang' => 'subtotals',
 							'label' => $langs->trans('Subtotal'),
 							'perm' => true,
+							'enabled' => (isModEnabled('order') && $object->status == Propal::STATUS_DRAFT),
 							'urlraw' => $_SERVER["PHP_SELF"].'?id='.$object->id.'&action=add_subtotal_line&token='.newToken()
 						),);
 					print dolGetButtonAction('', $langs->trans('AddSubtotalLine'), 'default', $url_button, '', true);
